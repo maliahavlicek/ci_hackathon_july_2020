b2f265389dd320bb7669d9a8b90e9581890a3339
maliahavlicek <malia.havlicek@gmail.com>
2020-07-02 20:31:22 -0600

    updated allowed hosts

--- ci_hackathon_july_2020/settings.py
+++ ci_hackathon_july_2020/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.environ.get('HOSTNAME'), "127.0.0.1", "adv-b-wall.herokuapp.com"]
 
 # Application definition
 
