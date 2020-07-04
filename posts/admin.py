7438ff8bea7510c27f199d5bf3bec303e07e97a5
Djang <django.heimgartner@gmail.com>
2020-07-04 15:45:31 +0200

    Added the reaction model to Admin

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Post, Reaction
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'datetime'
+    )
+    search_fields = (
+        'user',
+    )
+
+
+admin.site.register(Post, PostAdmin)
+
+
+class ReactionAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+    )
+
+
+admin.site.register(Reaction, ReactionAdmin)
