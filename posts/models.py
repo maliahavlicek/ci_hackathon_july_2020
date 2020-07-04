b5756330fe44c14dee83381dd54ed855cd1d7320
Djang <django.heimgartner@gmail.com>
2020-07-04 14:45:43 +0200

    Created the Post model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,19 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    """
+    User createdable posts.
+    """
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    status = models.TextField(max_length=250)
+    placed = models.BooleanField(default=False)
+    reations = models.CharField(max_length=25)
+    datetime = models.DateTimeField(auto_now_add=True)
+    photo = models.ImageField(
+        upload_to='posts',
+        null=True,
+        blank=True
+    )
