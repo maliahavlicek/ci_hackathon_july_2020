884b97a85a8244828e653d586888cd334b35b61c
Djang <django.heimgartner@gmail.com>
2020-07-04 14:31:10 +0200

    Created the admin models to be viewable in the admin pages

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import UserExtended, Family
 
-# Register your models here.
+
+class UserExtendedAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'profile_picture'
+    )
+    search_fields = (
+        'user',
+    )
+
+
+admin.site.register(UserExtended, UserExtendedAdmin)
+
+
+class FamilyAdmin(admin.ModelAdmin):
+    list_display = (
+        'family_name',
+        'hero_image'
+    )
+    search_fields = (
+        'family_name',
+    )
+
+
+admin.site.register(Family, FamilyAdmin)
