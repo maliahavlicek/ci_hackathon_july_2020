26b6a96339c1da1f09577cc0f64792fd3ccd4a95
Djang <django.heimgartner@gmail.com>
2020-07-04 13:42:17 +0200

    Flake8 beautified code

--- ci_hackathon_july_2020/settings.py
+++ ci_hackathon_july_2020/settings.py
@@ -16,8 +16,11 @@ from django.contrib.messages import constants as messages
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if os.path.exists('env.py'):
-    import env
-    # can override local to False here if you want to test things like 404, 500 error
+    import env  # noqa: F401
+    """
+    can override local to False here if you want to
+    test things like 404, 500 error
+    """
     DEBUG = True
 else:
     DEBUG = False
@@ -31,7 +34,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-ALLOWED_HOSTS = [os.environ.get('HOSTNAME'), "127.0.0.1", "adv-b-wall.herokuapp.com"]
+ALLOWED_HOSTS = [os.environ.get(
+    'HOSTNAME'), "127.0.0.1", "adv-b-wall.herokuapp.com"]
 DEFAULT_DOMAIN = 'https://{}'.format(ALLOWED_HOSTS[0])
 # Application definition
 
@@ -44,7 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_forms_bootstrap',
     'crispy_forms',
-    "django_nose",
+    'django_nose',
     'accounts',
     'storages',
 ]
@@ -66,8 +70,7 @@ ROOT_URLCONF = 'ci_hackathon_july_2020.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
-        ,
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -99,18 +102,18 @@ DATABASES = {
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+  {
+    'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+  },
+  {
+    'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+  },
+  {
+    'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+  },
+  {
+    'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+  },
 ]
 
 AUTHENTICATION_BACKENDS = [
@@ -143,7 +146,8 @@ AWS_STORAGE_BUCKET_NAME = 'ci-hackathon-july-2020'
 AWS_S3_REGION_NAME = 'us-east-2'
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.%s.amazonaws.com' % (AWS_STORAGE_BUCKET_NAME, AWS_S3_REGION_NAME)
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.%s.amazonaws.com' % (
+    AWS_STORAGE_BUCKET_NAME, AWS_S3_REGION_NAME)
 
 STATICFILES_LOCATION = 'static'
 STATICFILES_STORAGE = 'custom_storages.StaticStorage'
@@ -160,9 +164,6 @@ MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
-
-
-
 
 
 # SMTP Email configuration
