c319cd372ff9fb0ced3bf043f3e7a4ab78eb2f55
Djang <django.heimgartner@gmail.com>
2020-07-04 14:30:27 +0200

    Created the models for UserExtended & Family

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,34 @@
+from django.conf import settings
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserExtended(models.Model):
+    """
+    The user extended model to create a
+    new user and extend it with some extra field options.
+    A extended user can only have 1 user & user only 1 extended user
+    """
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    profile_picture = models.ImageField(
+        upload_to='images',
+        null=True,
+        blank=True
+    )
+
+
+class Family(models.Model):
+    """
+    A model for the family
+    Users can be added to more then one family and visa versa.
+    """
+    family_name = models.CharField(max_length=25)
+    members = models.ManyToManyField(settings.AUTH_USER_MODEL)
+    hero_image = models.ImageField(
+        upload_to='images',
+        null=True,
+        blank=True
+    )
+
+    def __str__(self):
+        return self.family_name
