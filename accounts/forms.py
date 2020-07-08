db854973b0c2eabb9f800d2e15fc2e84d7a5b295
maliahavlicek <malia.havlicek@gmail.com>
2020-07-08 06:52:44 -0600

    refactor update family so people can only remove themselves.

--- accounts/forms.py
+++ accounts/forms.py
@@ -100,6 +100,7 @@ class CreateFamilyForm(forms.Form):
     family_name = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control"}))
     hero_image = forms.ImageField(label="Hero Image")
     members = forms.CharField(widget=forms.HiddenInput(), required=False)
+    orig_members = forms.CharField(widget=forms.HiddenInput(), required=False)
 
     class Meta:
         model = Family
